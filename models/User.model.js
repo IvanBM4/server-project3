249821ef193bfc3d92adf78580aac74d77eaa98d
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-12-01 15:03:54 +0100

    nothing

--- models/User.model.js
+++ models/User.model.js
@@ -6,23 +6,45 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Email is required.'],
       unique: true,
-      minlength: [6, "Email must have at least 6 characters"]
+      minlength: [6, 'Email must have at least 6 characters']
+
     },
+
     password: {
       type: String,
       required: [true, 'Password is required.']
     },
+
     username: {
       type: String,
-      required: [true, "Username is required."],
-      minlength: [3, "Username must have at least 3 characters"]
+      required: [true, 'Username is required'],
+      minlength: [3, ' Username must have at least 3 characters'],
+      unique: true,
+      trim: true
+    },
+
+    avatar: {
+      type: String,
+      default: 'https://example.com/default-avatar.png',
+    },
+
+    grade: {
+      type: String,
+      required: true,
+    },
+
+    rol: {
+      type: String,
+      enum: ['Admin', 'User'],
+      default: 'User'
     }
+
   },
   {
     timestamps: true
   }
 );
 
-const User = model("User", userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
