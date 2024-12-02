30f599468f3de2bf992571d33b213bc5d28b00f8
Mercedes <mrcdssebastian@gmail.com>
2024-12-02 11:10:43 +0100

    activities get

--- routes/activities.routes.js
+++ routes/activities.routes.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const {
-    getActivities, getOneActivity, saveActivity, editActivity, removeActivity
+    getActivities,
+    getOneActivity,
+    saveActivity,
+    editActivity,
+    removeActivity,
+    filterActivities
 } = require('../controllers/activity.controllers')
+
 const router = express.Router()
+
+router.get('/activities/search', filterActivities)
 
 router.get('/activities', getActivities)
 
@@ -13,5 +21,6 @@ router.post('/activities/', saveActivity)
 router.put('/activities/:id', editActivity)
 
 router.delete('/activities/:id', removeActivity)
+
 
 module.exports = router
