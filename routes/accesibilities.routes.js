299dbfb724e0f8ae36470fa72917c0f1edfb3740
Ivan Blanco <ivanblanco4m@gmail.com>
2024-12-05 13:15:38 +0100

    fix accesibility

--- routes/accesibilities.routes.js
+++ routes/accesibilities.routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
 const {
-    getExistentAccesibilities
+    getExistentAccesibilities,
+    getAllowedAccesibilities
 } = require('../controllers/accesibilities.controllers')
 
 const router = express.Router()
 
 router.get('/activities/accesibilities', getExistentAccesibilities)
+
+router.get('/accesibilities-allowed', getAllowedAccesibilities)
 
 module.exports = router
